94c8f678793a8ff4408f0ab55f37029eb2aef0cb
matthieu <matthieu@matthieus-mbp.fios-router.home>
2020-02-04 09:31:40 -0500

    New commit

---
+++ src/PasswordCheckerUtility.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author matthieu
+ */
+public class PasswordCheckerUtility {
+  
+    /*
+    
+        Return true if valid, returns false if an invalid password
+        Pseudocode will be: 
+           Take in a String parameter containing the password
+           Check the status of the password(valid or invalid) 
+           Return status (as a boolean value) 
+    
+        Our passsword is valid if it meets all the conditions highlighted in the project specification
+    
+    
+    */
+    public static boolean isValidPassword(String passwordString) {
+    
+        
+        
+    }
+    
+}
